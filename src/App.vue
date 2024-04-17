03ff796ac5583feee3be87fb8e47a2f8fc2761b7
sisdream <sissipascale2@gmail.com>
2024-04-17 23:48:41 +0200

    fix: aggiusti struttura base e card

--- src/App.vue
+++ src/App.vue
@@ -85,46 +85,38 @@ export default {
     <h1 class="text-center">{{ title }}</h1>
     
     <!-- BOTTONE -->
-    <div class="text-center my-4">
-      <!-- <button  class="btn btn-success me-5" @click="random()">GIOCA TU!</button> -->
+    <div class="text-center my-2">
       <button class="btn btn-success me-5" @click="handleStart('player')">PESCA UNA CARTA!</button>
       <button class="btn btn-warning" @click="handleStart('pc')">PESCA LA CARTA DEL PC!</button>
     </div>
 
     <!-- CARD -->
-    <div class="row g-3 mt-3">
+    <div class="row g-3 d-flex justify-content-center align-items-center">
       <div class="col-5 d-flex flex-column align-items-center">
-        <h4>Card Giocatore</h4>
-        <div v-if="userCard" class="card w-100">
-          <div class="card-header">
-            <h5 class="card-title">{{ userCard.name }}</h5>
+        <h4>USER</h4>
+        <div v-if="userCard" class="card bg-transparent w-100">
+          <div class="card-header p-0">
+            <h5 class="card-title text-center fs-1">{{ userCard.name }}</h5>
           </div>
-          <div class="card-body">            
+          <div class="card-body p-0">            
+           <div class="image">
             <img :src="`http://127.0.0.1:8000/storage` + userCard.type.image" class="w-100" alt="">
-            <!-- <p class="card-text">
-              {{ userCard.description }}
-            </p> -->
-
+           </div>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                <strong>Attacco: </strong>{{ userCard.strength }}
+              <li class="list-group-item p-0 bg-transparent ">
+                <strong class="fs-4">Attacco: {{ userCard.strength }}</strong>
               </li>
-              <li class="list-group-item">
-                <strong>Difesa: </strong>{{ userCard.defence }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Difesa: {{ userCard.defence }}</strong>
               </li>
-              <li class="list-group-item">
-                <strong>Velocità: </strong>{{ userCard.speed }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Velocità: {{ userCard.speed }}</strong>
               </li>
-              <li class="list-group-item">
-                <strong>Vita: </strong>{{ userCard.life }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Vita: {{ userCard.life }}</strong>
               </li>
             </ul>
           </div>
-
-          <!-- <div class="card-footer">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Return link</a>
-          </div> -->
         </div>
 
         <div v-else>
@@ -132,54 +124,43 @@ export default {
         </div>
       </div>
 
-      <div class="col-2 d-flex justify-content-center">
+      <div class="col-2">
         <div>
           <button @click="fight()" class="text-center btn btn-danger">FIGHT!</button>
         </div>
       </div>
 
       <div class="col-5 d-flex flex-column align-items-center">
-        <h4>Card Computer</h4>
-        <div v-if="pcCard" class="card w-100">
-          <div class="card-header">
-            <h5 class="card-title">{{ pcCard.name }}</h5>
+        <h4>COMPUTER</h4>
+        <div v-if="pcCard" class="card bg-transparent w-100">
+          <div class="card-header p-0">
+            <h5 class="card-title text-center fs-1">{{ pcCard.name }}</h5>
           </div>
-
-          <div class="card-body">
-            <img :src="`http://127.0.0.1:8000/storage` + pcCard.type.image" class="w-100" alt="">
-
-            <!-- <p class="card-text">
-              {{ pcCard.description }}
-            </p> -->
-
+          <div class="card-body p-0">
+            <div class="image">
+              <img :src="`http://127.0.0.1:8000/storage` + pcCard.type.image" class="w-100" alt="">
+            </div>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                <strong>Attacco: </strong>{{ pcCard.strength }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Attacco: {{ pcCard.strength }}</strong>
               </li>
-              <li class="list-group-item">
-                <strong>Difesa: </strong>{{ pcCard.defence }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Difesa: {{ pcCard.defence }}</strong>
               </li>
-              <li class="list-group-item">
-                <strong>Velocità: </strong>{{ pcCard.speed }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Velocità: {{ pcCard.speed }}</strong>
               </li>
-              <li class="list-group-item">
-                <strong>Vita: </strong>{{ pcCard.life }}
+              <li class="list-group-item p-0 bg-transparent">
+                <strong class="fs-4">Vita: {{ pcCard.life }}</strong>
               </li>
             </ul>
           </div>
-
-          <!-- <div class="card-footer">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Return link</a>
-          </div> -->
         </div>
 
         <div v-else>
           <p>nessuna carta scelta</p>
         </div>
       </div>
-
-      
     </div>
   </div>
 </template>
